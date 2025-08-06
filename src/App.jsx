dc540df5e00828b9f09165854e67699b5d054a19
Óscar Andrés Moreno Díaz <123341926+TresMilDeName22@users.noreply.github.com>
2025-08-05 22:01:39 -0500

    No, Prueba Oski 4

--- src/App.jsx
+++ src/App.jsx
@@ -92,7 +92,7 @@ function Landing() {
         >
           {t('hero.subtitle', 'Your business deserves a stellar online presence. We’ll help you reach for the stars.')}
         </motion.p>
-        <DiscoverButton to="services" className="btn shadow-md active:shadow-lg"></DiscoverButton>
+        <DiscoverButton to="services" class="btn shadow-md active:shadow-lg"></DiscoverButton>
       </section>
 
       {/* About Us Section */}
